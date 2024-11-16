7f418e4a8bcdb14a31abab88403691b9b3e6d5bf
Lobobranco <rntt2511@gmail.com>
2024-11-16 09:05:21 -0300

    nova atualização

--- minio/bucket-init.sh
+++ minio/bucket-init.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
-# Aguarda o servidor MinIO estar ativo
-sleep 10
+echo "Aguardando MinIO estar ativo..."
+while ! curl -s http://localhost:9000/minio/health/live > /dev/null; do
+    echo "MinIO ainda não está ativo. Tentando novamente em 3 segundos..."
+    sleep 3
+done
+echo "MinIO está ativo."
 
-# Configura o alias para o MinIO client usando as variáveis de ambiente
+echo "Configurando alias para o MinIO client..."
 mc alias set myminio http://localhost:9000 "$MINIO_ROOT_USER" "$MINIO_ROOT_PASSWORD"
 
-# Cria os buckets necessários
-mc mb myminio/landing
-mc mb myminio/bronze
-mc mb myminio/silver
-mc mb myminio/gold
+echo "Criando buckets..."
+mc mb myminio/landing || true
+mc mb myminio/bronze || true
+mc mb myminio/silver || true
+mc mb myminio/gold || true
 
-# Copia os arquivos CSV da pasta 'include' para o bucket 'landing'
-mc cp /minio/include/marca_carro.csv myminio/landing/marca_carro.csv
+echo "Copiando arquivos CSV para o bucket 'landing'..."
+mc cp /minio/include/marca_carro.csv myminio/landing/marca_carro.csv || true
+
+echo "Script finalizado com sucesso!"
