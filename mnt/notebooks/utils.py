cf32e52f30c4b7ee535c49d05215911614bcb4e7
Lobobranco <rntt2511@gmail.com>
2024-12-06 11:52:05 -0300

    nova atualização

--- mnt/notebooks/utils.py
+++ mnt/notebooks/utils.py
@@ -2,53 +2,52 @@ import pyspark
 from pyspark.sql import SparkSession
 import os
 
-AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
-S3_ENDPOINT = os.getenv("S3_ENDPOINT")
-NESSIE_URI = os.getenv("NESSIE_URI")
-
 def get_spark_session(spark_app, bucket):
-    MASTER = "spark://spark-master:7077"
-    jar_packages = [
-        "org.apache.iceberg:iceberg:spark-runtime-3.5_2.12:1.6.1",
-        "org.projectnessie:nessie:spark-extensions-3.5_2.12:0.99.0",
-        "software.amazon.awssdk:bundle-2.28.13",
-        "software.amazon.awssdk:url-connection-client-2.28.13",
-        "org.apache.iceberg:iceberg-aws-1.6.1"
-    ]
 
-    spark_extensions = [
-        "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
-        "org.projectnessie.spark.extensions.NessieSparkSessionExtensions"
-    ]
+  MASTER = "spark://spark-master:7077"
+  jar_packages = [
+    "org.apache.iceberg:iceberg-spark-runtime-3.5_2.12:1.6.1",
+    "org.projectnessie.nessie-integrations:nessie-spark-extensions-3.5_2.12:0.99.0",
+    #"software.amazon.awssdk:bundle:2.28.13",
+    #"software.amazon.awssdk:url-connection-client:2.28.13",
+    "org.apache.iceberg:iceberg-aws-bundle:1.6.1"
+  ]
 
-    conf = (
-        pyspark.SparkConf()
-        .setAppName(spark_app)
-        .set("spark.master", MASTER)
-        .set("spark.jars.repositories", "https://repo.maven.apache.org/maven2,https://oss.sonatype.org/content/repositories/releases")
-        .set("spark.jars.packages", ','.join(jar_packages))
-        .set("spark.sql.extensions", ','.join(spark_extensions))
-        .set("spark.executor.memory", "1g")
-        .set("spark.executor.cores", "1")
-        .set("spark.executor.instances", "1")
-        .set("spark.driver.memory", "1g")
-        .set("spark.sql.catalog.nessie", "org.apache.iceberg.spark.SparkCatalog")
-        .set("spark.sql.catalog.nessie.s3.path-style-access", "true")
-        .set("spark.sql.catalog.nessie.s3.endpoint", S3_ENDPOINT)
-        .set("spark.sql.catalog.nessie.warehouse", f"s3://{bucket}/")
-        .set("spark.sql.catalog.nessie.catalog-impl", "org.apache.iceberg.nessie.NessieCatalog")
-        .set("spark.sql.catalog.nessie.io-impl", "org.apache.iceberg.aws.s3.S3FileIO")
-        .set("spark.sql.catalog.nessie.uri", NESSIE_URI)
-        .set("spark.sql.catalog.nessie.ref", "main")
-        .set("spark.sql.catalog.nessie.authentication.type", "NONE")
-        .set("spark.sql.catalog.nessie.cache-enabled", "false")
-        .set("spark.hadoop.fs.s3a.access.key", AWS_ACCESS_KEY)
-        .set("spark.hadoop.fs.s3a.secret.key", AWS_SECRET_KEY)
-        .set("spark.hadoop.fs.s3a.endpoint", S3_ENDPOINT)
-        .set("spark.hadoop.fs.s3a.path.style.access", "true")
-    )
+  spark_extensions = [
+    "org.apache.iceberg.spark.extensions.IcebergSparkSessionExtensions",
+    "org.projectnessie.spark.extensions.NessieSparkSessionExtensions"
+  ]
 
-    spark = SparkSession.builder.config(conf=conf).getOrCreate()
-    spark.sparkContext.setLogLevel("ERROR")
-    return spark
+  AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY_ID")
+  AWS_SECRET_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
+  MINIO_HOST = os.getenv("S3_ENDPOINT")
+  NESSIE_URI = os.getenv("NESSIE_URI")
+
+  conf = (
+    pyspark.SparkConf()
+    .setAppName('iceberg')
+    .set("spark.master", MASTER)
+    .set('spark.jars.packages', ','.join(jar_packages))
+    .set('spark.sql.extensions', ','.join(spark_extensions))
+    .set("spark.executor.instances", "1")
+    .set("spark.driver.memory", "2g")
+    .set('spark.sql.catalog.nessie', "org.apache.iceberg.spark.SparkCatalog")
+    .set('spark.sql.catalog.nessie.s3.path-style-access', 'true')
+    .set('spark.sql.catalog.nessie.s3.endpoint', MINIO_HOST)
+    .set("spark.sql.catalog.nessie.warehouse", f"s3://{bucket}/")
+    .set('spark.sql.catalog.nessie.catalog-impl', 'org.apache.iceberg.nessie.NessieCatalog')
+    .set('spark.sql.catalog.nessie.io-impl', 'org.apache.iceberg.aws.s3.S3FileIO')
+    .set('spark.sql.catalog.nessie.uri', NESSIE_URI)
+    .set('spark.sql.catalog.nessie.ref', 'main')
+    .set('spark.sql.catalog.nessie.authentication.type', 'NONE')
+    .set('spark.sql.catalog.nessie.cache-enabled', 'false')
+    .set("spark.hadoop.fs.s3a.access.key", AWS_ACCESS_KEY)
+    .set("spark.hadoop.fs.s3a.secret.key", AWS_SECRET_KEY)
+    .set("spark.hadoop.fs.s3a.endpoint", MINIO_HOST)
+    #
+    # .set("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
+  )
+  spark = SparkSession.builder.config(conf=conf).getOrCreate()
+  spark.sparkContext.setLogLevel("ERROR")
+  return spark
